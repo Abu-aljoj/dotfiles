b577748cd942e60ad0f8092bde8d4abcf60fdd74
Majd Abu Diab <majdabudiab2013@gmail.com>
2025-07-15 12:49:18 +0300

    Added hyprpanel and nwg-drawer configs

--- .config/hypr/Scripts/toggleOpacity.sh
+++ .config/hypr/Scripts/toggleOpacity.sh
@@ -4,9 +4,11 @@
 STATE=$(hyprctl -j getoption decoration:active_opacity | jq ".float")
 
 if [ "${STATE}" == "0.900000" ]; then
-	hyprctl keyword decoration:active_opacity 1
-	hyprctl keyword decoration:inactive_opacity 1
+  hyprctl keyword decoration:active_opacity 1
+  hyprctl keyword decoration:inactive_opacity 1
+  hyprctl keyword decoration:dim_inactive false
 else
-	hyprctl keyword decoration:active_opacity 0.9
-	hyprctl keyword decoration:inactive_opacity 0.7
+  hyprctl keyword decoration:active_opacity 0.9
+  hyprctl keyword decoration:inactive_opacity 0.9
+  hyprctl keyword decoration:dim_inactive true
 fi
